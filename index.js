0666bb7a16d19160600725a0776b11b18a68508e
la-moche <priamus.rex@gmail.com>
2025-10-25 11:32:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', function(event) {
-  event.preventDefault(); // предотвращаем стандартное поведение кнопки
-  alert('Извините, я ещё не доработал эту кнопку.\nСвяжитесь со мной через Telegram по ссылке вверху.');
+  event.preventDefault();
+  alert('Sorry, I haven\'t finished this button yet.\nContact me via Telegram using the link at the top.');
 });
 
 
